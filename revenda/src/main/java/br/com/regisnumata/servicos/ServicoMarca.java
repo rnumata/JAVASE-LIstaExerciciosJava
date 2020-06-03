9e7f24c82eb5a57ec4e9d8276ed7589adb326f48
rnumata <regis.numata@gmail.com>
2020-06-03 09:32:49 -0300

    Add files via upload

--- revenda/src/main/java/br/com/regisnumata/servicos/ServicoMarca.java
+++ revenda/src/main/java/br/com/regisnumata/servicos/ServicoMarca.java
@@ -1,8 +1,12 @@
 package br.com.regisnumata.servicos;
 
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
 
 import br.com.regisnumata.entidade.Marca;
 import br.com.regisnumata.util.JPAUtils;
@@ -17,24 +21,102 @@ import br.com.regisnumata.util.JPAUtils;
 @RequestScoped
 public class ServicoMarca {
 	
+	
+	/**
+	 * Cadastra uma nova marca
+	 * @param marca
+	 * @return
+	 * @throws Exception
+	 */
 	public Marca cadastrar(Marca marca) throws Exception {
+		
+		EntityManager em = JPAUtils.getInstance();
+		
+		Marca marcaExistente = buscarPorNome(marca.getNome());
+		
+		if (marcaExistente != null) {
+			throw new Exception("Marca já existe!");
+		}else {
+			try {
+				em.getTransaction().begin();
+				em.persist(marca);
+				em.getTransaction().commit();
+				return marca;
+			} catch (Exception e) {
+				em.getTransaction().rollback();
+				throw new Exception("Falha no cadastro" + e.getMessage());
+			}
+		}// fim if
+		
+	}
+	
+	
+	/**
+	 * Exclui um registro da marca
+	 * @param marca
+	 * @throws Exception
+	 */
+	public void excluir(Marca marca) throws Exception {
 		
 		EntityManager em = JPAUtils.getInstance();
 		
 		try {
 			em.getTransaction().begin();
-			em.persist(marca);
+			em.remove(em.merge(marca));
 			em.getTransaction().commit();
-			return marca;
 		} catch (Exception e) {
 			em.getTransaction().rollback();
-			throw new Exception("Falha no cadastro" + e.getMessage());
+			throw new Exception("Falha na exclusão" + e.getMessage());
 		}
+	}
+	
+	
+	/**
+	 * Edita uma marca selecionada
+	 * @param marca
+	 * @throws Exception
+	 */
+	public void editarMarca(Marca marca) throws Exception {
 		
+		EntityManager em = JPAUtils.getInstance();
+		
+		try {
+			em.getTransaction().begin();
+			em.merge(marca);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+			throw new Exception("Falha na edição. Erro:" + e.getMessage());
+		}
 		
 	}
 	
 	
+	/**
+	 * Lista todos os registros
+	 * @return
+	 */
+	public List<Marca> listar(){
+		EntityManager em = JPAUtils.getInstance();
+		Query query = em.createQuery("FROM Marca m");
+		return query.getResultList();
+	}
 	
-
-}
+	
+	/**
+	 * Busca a marca por nome
+	 * @param nome
+	 * @return marca ou Null se não encontrar
+	 */
+	
+	public Marca buscarPorNome(String nome) {
+		EntityManager em = JPAUtils.getInstance();
+		Query query = em.createQuery("FROM Marca m WHERE m.nome = :p1").setParameter("p1", nome);
+		try {
+			return (Marca) query.getSingleResult();
+		}catch (NoResultException e) {
+			return null;
+		}
+	}
+	
+}// fim classe
--- revenda/src/main/java/br/com/regisnumata/view/MarcaBean.java
+++ revenda/src/main/java/br/com/regisnumata/view/MarcaBean.java
@@ -1,5 +1,6 @@
 package br.com.regisnumata.view;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -10,6 +11,7 @@ import br.com.regisnumata.entidade.Marca;
 import br.com.regisnumata.servicos.ServicoMarca;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @ViewScoped
@@ -19,22 +21,66 @@ public class MarcaBean implements Serializable {
 
 	private Marca marca;
 	
+	private List<Marca> marcas;
+	
 	@Inject
 	private ServicoMarca servicoMarca;
+	
 
 	public MarcaBean() {
+		
+	}
+	
+	@PostConstruct
+	public void init() {
 		this.marca = new Marca();
+		this.atualizarMarcas();
+	}
+	
+	private void atualizarMarcas() {
+		this.marcas = this.servicoMarca.listar();
 	}
 	
 	public void cadastrar() {
 		try {
 			this.servicoMarca.cadastrar(marca);
 			this.marca = new Marca();
+			atualizarMarcas();
 			Messages.addGlobalInfo("Cadastro Ok!");
 		} catch (Exception e) {
 			Messages.addGlobalError(e.getMessage());
 		}
 	}
+	
+	public void excluir(Marca marca) {
+		try {
+			this.servicoMarca.excluir(marca);
+			this.atualizarMarcas();
+			Messages.addGlobalInfo("Excluído com sucesso!");
+		} catch (Exception e) {
+			Messages.addGlobalError(e.getMessage());
+		}
+	}
+	
+	public void editar(Marca marca) {
+		this.marca = marca;
+	}
+	
+	public void editarMarca () {
+		try {
+			this.servicoMarca.editarMarca(this.marca);
+			this.marca = new Marca();
+			Messages.addGlobalInfo("Marca Atualizada!");
+		} catch (Exception e) {
+			Messages.addGlobalError(e.getMessage());
+		}
+	}
+	
+	public void cancelarEdicao() {
+		this.marca = new Marca();
+	}
+	
+
 	
 	public Marca getMarca() {
 		return marca;
@@ -52,8 +98,13 @@ public class MarcaBean implements Serializable {
 		this.servicoMarca = servicoMarca;
 	}
 	
-	
-	
+	public List<Marca> getMarcas() {
+		return marcas;
+	}
+
+	public void setMarcas(List<Marca> marcas) {
+		this.marcas = marcas;
+	}
 	
 	
 	
